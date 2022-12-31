e98c6192c0eef5887b20d783643b9148acde58f5
Claudio Alejo <calejoalar@gmail.com>
2022-12-30 20:10:04 -0500

    v.1.1

--- dev/filedev.js
+++ dev/filedev.js
@@ -1,1 +1,36 @@
+// Ejemplo de imprimir
+console.log("----------------");
 console.log("Hola a todos. Soy Claudio");
+
+// Operador de Invertir
+console.log("----------------");
+let a = 9;
+for(let i=0;i<=10;i++)
+    console.log(~i);
+
+// Operador para eliminar una propiedad
+console.log("----------------");
+let b = {
+    prop1: 23,
+    prop2: "test"
+};
+console.log(b);
+delete b.prop1;
+console.log(b);
+
+// Operador para saber el tipo de dato
+console.log("----------------");
+let c = 33;
+let d = {
+    prop1: 33
+}
+let e = [3,4, "dd"];
+let f = true;
+
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+
+
+
